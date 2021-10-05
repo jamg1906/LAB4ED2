c7a2b7f043d3e8da88dbe8040bb4c876c658b09f
Mario Jose Roldan <mariojudoka@gmail.com>
2021-10-04 23:57:38 -0600

    cesar

--- Lab04_EDII/BibliotecaDeClases/Cifrados/Cesar.cs
+++ Lab04_EDII/BibliotecaDeClases/Cifrados/Cesar.cs
@@ -1,79 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Linq;
+using BibliotecaDeClases.Interfaces;
 namespace BibliotecaDeClases.Cifrados
 {
-    public class Cesar: Interfaces.IAlgoritmoCifrado
+    public class Cesar : IAlgoritmoCifrado
     {
-        private static Dictionary<int, int> DirAlpha = new Dictionary<int, int>();
-        private static string routeDirectory = Environment.CurrentDirectory;
+        List<byte> Alphabet_Uppercase = new List<byte>() { 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90 };
+        List<byte> Alphabet_Lowercase = new List<byte>() { 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122 };
 
-        static void CollectDic(string key, int opc)
+        List<byte> Alphabet_Disorganized_Uppercase = new List<byte>();
+        List<byte> Alphabet_Disorganized_Lowercase = new List<byte>();
+
+        public byte[] DescifrarData(byte[] content, string key)
         {
-            DirAlpha = new Dictionary<int, int>();
-            key = key.ToUpper();
-            var CountOriginal = 65;
-            var CountNew = 65;
+            SetAlphabets(ConvertToByte(key));
 
-            if (opc == 1)
+            List<byte> result = new List<byte>(content.Length);
+            for (int i = 0; i < content.Length; i++)
             {
-                do
+                if (Alphabet_Disorganized_Uppercase.Contains(content[i]))
                 {
-                    if (key.Length > 0)
-                    {
-                        if (!DirAlpha.ContainsValue(key[0]))
-                        {
-                            DirAlpha.Add(CountOriginal, key[0]);
-                            CountOriginal++;
-                        }
-                        key = key.Substring(1, key.Length - 1);
-                    }
-                    else
-                    {
-                        if (!DirAlpha.ContainsValue(CountNew))
-                        {
-                            DirAlpha.Add(CountOriginal, CountNew);
-                            CountOriginal++;
-                        }
-                        CountNew++;
-                    }
-                } while (CountOriginal < 91);
-            }
-            else
-            {
-                do
+                    int position = Alphabet_Disorganized_Uppercase.IndexOf(content[i]);
+                    result.Add(Alphabet_Uppercase[position]);
+                }
+                else if (Alphabet_Disorganized_Lowercase.Contains(content[i]))
                 {
-                    if (key.Length > 0)
-                    {
-                        if (!DirAlpha.ContainsKey(key[0]))
-                        {
-                            DirAlpha.Add(key[0], CountOriginal);
-                            CountOriginal++;
-                        }
-                        key = key.Substring(1, key.Length - 1);
-                    }
-                    else
-                    {
-                        if (!DirAlpha.ContainsKey(CountNew))
-                        {
-                            DirAlpha.Add(CountNew, CountOriginal);
-                            CountOriginal++;
-                        }
-                        CountNew++;
-                    }
-                } while (CountOriginal < 91);
+                    int position = Alphabet_Disorganized_Lowercase.IndexOf(content[i]);
+                    result.Add(Alphabet_Lowercase[position]);
+                }
+                else
+                {
+                    result.Add(content[i]);
+                }
             }
+            return result.ToArray();
         }
 
-        public byte[] CifrarData(byte[] contenido, string llave)
+        public byte[] CifrarData(byte[] content, string key)
         {
-            throw new NotImplementedException();
+            SetAlphabets(ConvertToByte(key));
+
+            List<byte> result = new List<byte>(content.Length);
+            for (int i = 0; i < content.Length; i++)
+            {
+                if (Alphabet_Uppercase.Contains(content[i]))
+                {
+                    int position = Alphabet_Uppercase.IndexOf(content[i]);
+                    result.Add(Alphabet_Disorganized_Uppercase[position]);
+                }
+                else if (Alphabet_Lowercase.Contains(content[i]))
+                {
+                    int position = Alphabet_Lowercase.IndexOf(content[i]);
+                    result.Add(Alphabet_Disorganized_Lowercase[position]);
+                }
+                else
+                {
+                    result.Add(content[i]);
+                }
+            }
+            return result.ToArray();
         }
 
-        public byte[] DescifrarData(byte[] contenido, string llave)
+        public void SetAlphabets(byte[] key)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < key.Length; i++)
+            {
+                string Upper_letter = Convert.ToString((char)key[i]).ToUpper();
+                string Lower_letter = Convert.ToString((char)key[i]).ToLower();
+
+                byte UpperLetter = Convert.ToByte(Convert.ToChar(Upper_letter));
+                Alphabet_Disorganized_Uppercase.Add(UpperLetter);
+
+                byte LowerLetter = Convert.ToByte(Convert.ToChar(Lower_letter));
+                Alphabet_Disorganized_Lowercase.Add(LowerLetter);
+            }
+            for (int i = 0; i < Alphabet_Uppercase.Count; i++)
+            {
+                if (!Alphabet_Disorganized_Uppercase.Contains(Alphabet_Uppercase[i]))
+                {
+                    Alphabet_Disorganized_Uppercase.Add(Alphabet_Uppercase[i]);
+                }
+                if (!Alphabet_Disorganized_Lowercase.Contains(Alphabet_Lowercase[i]))
+                {
+                    Alphabet_Disorganized_Lowercase.Add(Alphabet_Lowercase[i]);
+                }
+            }
+        }
+        public static byte[] ConvertToByte(string content)
+        {
+            byte[] array = new byte[content.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToByte(content[i]);
+            }
+            return array;
         }
     }
-}
+
+    }
